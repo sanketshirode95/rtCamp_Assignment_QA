de36506d4b7cbad5e89b8d1e20e89023ac41bfbc
Sanket Shirode <101618038+sanketshirode95@users.noreply.github.com>
2024-02-19 14:31:54 +0530

    Update SearchProduct.java

--- AmazonTestPlaywright/src/test/java/amazon/SearchProduct.java
+++ AmazonTestPlaywright/src/test/java/amazon/SearchProduct.java
@@ -13,26 +13,29 @@ public class SearchProduct
 		Playwright playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch(new LaunchOptions().setHeadless(false));
 		Page page = browser.newPage();
+
 		// Navigate to amazon login page
 		page.navigate("https://www.amazon.in/");
 		Locator myAccount = page.locator("//a[@id='nav-link-accountList']");
 		myAccount.hover();
-		page.click("//div[@id='nav-flyout-ya-signin']//span[@class='nav-action-inner'][normalize-space()='Sign in']");
+		page.click("//*[@id="nav-flyout-ya-signin"]/a/span");
 
 		// enter login details
-		page.fill("#ap_email", "7020801623");
-		page.click("#continue");
+		page.fill("//*[@name="email"]", "7020801623");
+		page.click("//*[@type="submit"]");
 
-		page.fill("#ap_password", "sanket1808");
-		page.click("#signInSubmit");
+		page.fill("//*[@name="password"]", "sanket1808");
+		page.click("//*[@id="auth-signin-button-announce"]");
+
 		
-		// Validate search result
+		// search product
 		String searchTerm = "sony ps5 console";
 		page.fill("#twotabsearchtextbox", searchTerm);
 		page.click("//input[@id='nav-search-submit-button']");
 		//page.waitForNavigation();
 		page.waitForTimeout(5000);
-		
+
+		//validate search product functionality
 		boolean searchResultValidated = page.isVisible("//span[contains(text(), 'Sony PS5 Console')]");
 	
 		if (searchResultValidated) {
