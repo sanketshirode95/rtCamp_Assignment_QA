0a137a59403239be96fc7e9baa8dd1e98a23f866
Sanket Shirode <101618038+sanketshirode95@users.noreply.github.com>
2024-02-19 14:27:39 +0530

    Update CheckoutProduct.java

--- AmazonTestPlaywright/src/test/java/amazon/CheckoutProduct.java
+++ AmazonTestPlaywright/src/test/java/amazon/CheckoutProduct.java
@@ -11,32 +11,31 @@ public class CheckoutProduct {
 
 	public static void main(String[] args) 
 	{
-	Playwright playwright=Playwright.create();
-        Browser browser= playwright.chromium().launch(new LaunchOptions().setHeadless(false));
-        Page page=browser.newPage();
-        Login_po login_po=new Login_po();
-		
-        // Navigate to amazon login page 
-        page.navigate("https://www.amazon.in/");
-        Locator myAccount=page.locator("//a[@id='nav-link-accountList']");
-        myAccount.hover();
-        page.click("//div[@id='nav-flyout-ya-signin']//span[@class='nav-action-inner'][normalize-space()='Sign in']");
+	        Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium().launch(new LaunchOptions().setHeadless(false));
+		Page page = browser.newPage();
+
+		// Navigate to amazon login page
+		page.navigate("https://www.amazon.in/");
+		Locator myAccount = page.locator("//a[@id='nav-link-accountList']");
+		myAccount.hover();
+		page.click("//*[@id="nav-flyout-ya-signin"]/a/span");
+
+		// enter login details
+		page.fill("//*[@name="email"]", "7020801623");
+		page.click("//*[@type="submit"]");
+
+		page.fill("//*[@name="password"]", "sanket1808");
+		page.click("//*[@id="auth-signin-button-announce"]");
         
-        //enter login details
-        page.fill("#ap_email", "7020801623");
-        page.click("#continue");
-        
-        page.fill("#ap_password", "sanket1808");
-        page.click("#signInSubmit");
-        
-        //search product and add to cart
-        page.fill("#twotabsearchtextbox", "playstation 5");
-        page.click("//input[@id='nav-search-submit-button']");
-        page.click("#a-autoid-1-announce");
+                 //search product and add to cart
+                 page.fill("//*[@placeholder="Search Amazon.in"]", "playstation 5");
+                 page.click("//*[@id='nav-search-submit-button']");
+                 page.click("//*[@type="submit"]");
        
-        //go to cart
-        page.click("//span[@id='nav-cart-count']");
-        page.click("//input[@value='Proceed to checkout']");
+                //go to cart
+                 page.click("//span[@id='nav-cart-count']");
+                 page.click("//input[@value='Proceed to checkout']");
         
 	}
 
